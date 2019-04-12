8a0ec17097b1192c5937a7eb45e319ffda1dced7
songdi <670165552@qq.com>
2019-04-12 15:16:02 +0800

    初始化

---
+++ src/main/java/com/songdi/springbootvue/SpringBootVueApplication.java
@@ -0,0 +1,13 @@
+package com.songdi.springbootvue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootVueApplication.class, args);
+	}
+
+}
