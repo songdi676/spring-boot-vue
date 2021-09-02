02ab63497987fcf9de0f03bedfb1e338d4123fa8
宋 迪 <670165552@qq.com>
2021-09-02 20:04:11 +0800

    sada

--- src/main/java/com/songdi/springbootvue/DemoController.java
+++ src/main/java/com/songdi/springbootvue/DemoController.java
@@ -27,7 +27,7 @@ public class DemoController {
         }
         logger.info("{} hello {}", address.getHostAddress(), objectId);
 
-        return address.getHostAddress() + " Hello " + objectId + " v2 \n";
+        return address.getHostAddress() + " Hello " + objectId + " v1 \n";
 
     }
 
@@ -45,7 +45,7 @@ public class DemoController {
         if (objectId % 2 == 0) {
             throw new RuntimeException("异常");
         }
-        return address.getHostAddress() + "Fusing Hello World " + objectId + " v2 \n";
+        return address.getHostAddress() + "Fusing Hello World " + objectId + " v1 \n";
 
     }
 
@@ -61,7 +61,7 @@ public class DemoController {
         logger.info("{} hello {}", address.getHostAddress(), objectId);
 
         Thread.sleep(objectId * 20);
-        return address.getHostAddress() + "Delayed Hello World " + objectId + " v2 \n";
+        return address.getHostAddress() + "Delayed Hello World " + objectId + " v1 \n";
 
     }
 }
